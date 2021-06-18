2f0c5530e91c8d9207d743547428499c4b714fcd
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-06-19 00:46:52 +0530

    fix:add row condition

--- global_custom/custom/js/purchase_order.js
+++ global_custom/custom/js/purchase_order.js
@@ -32,6 +32,7 @@ cur_frm.cscript.item_code = function (frm, cdt, cdn) {
 
 frappe.ui.form.on('Purchase Order Itemwise Rate Details', {
 	po_itemwise_rate_details_add: function(frm){
+    console.log('Add row clicked')
 		frm.set_df_property('po_itemwise_rate_details', 'hidden', 1);
 	}
 });
