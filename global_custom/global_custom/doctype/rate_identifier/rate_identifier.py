458dbc9e5bd5990725204449c57a172dbebf3c83
root <kaviyaperiyasamy22@gmail.com>
2021-06-19 08:49:17 +0000

    fix: after testing

--- global_custom/global_custom/doctype/rate_identifier/rate_identifier.py
+++ global_custom/global_custom/doctype/rate_identifier/rate_identifier.py
@@ -19,6 +19,7 @@ def fetch_rate_details(date, item):
     for row in po_details[::-1]:
         if frappe.db.get_value('Purchase Order', row.parent,'docstatus') == 1:
             po_doc = frappe.get_doc('Purchase Order', row.parent)
+            frappe.log_error(f'{getdate(date)}, {po_doc.transaction_date}')
             if po_doc.transaction_date == getdate(date):
                 po_rate_details.append(
                     {
