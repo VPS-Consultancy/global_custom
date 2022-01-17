d6fa919ecbc201bfba86a18c1567165a63b75f91
vivek <vivekchamp84@gmail.com>
2022-01-17 12:25:01 +0000

    Update Changes

---
+++ global_custom/global_custom/doctype/bank_reconciliation/bank_reconciliation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, VPS Consultancy and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Bank Reconciliation', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ global_custom/global_custom/doctype/bank_reconciliation/test_bank_reconciliation.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, VPS Consultancy and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestBankReconciliation(unittest.TestCase):
+	pass
